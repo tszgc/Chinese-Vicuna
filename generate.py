be38ff703f21b4f8df0d2cf27479b78d48eb0f64
zhaogc <zhaogc@fusionskye.com>
2023-06-05 16:50:40 +0800

    1

--- generate.py
+++ generate.py
@@ -15,7 +15,6 @@ from transformers import LlamaTokenizer, LlamaForCausalLM, GenerationConfig
 parser = argparse.ArgumentParser()
 parser.add_argument("--model_path", type=str, default="/model/13B_hf")
 parser.add_argument("--lora_path", type=str, default="checkpoint-3000")
-parser.add_argument("--use_typewriter", type=int, default=1)
 parser.add_argument("--use_local", type=int, default=1)
 args = parser.parse_args()
 print(args)
@@ -84,26 +83,6 @@ else:
     )
 
 
-def generate_prompt(instruction, input=None):
-    if input:
-        return f"""Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
-
-### Instruction:
-{instruction}
-
-### Input:
-{input}
-
-### Response:"""
-    else:
-        return f"""Below is an instruction that describes a task. Write a response that appropriately completes the request.
-
-### Instruction:
-{instruction}
-
-### Response:"""
-
-
 if not LOAD_8BIT:
     model.half()  # seems to fix bugs for some users.
 
@@ -123,8 +102,7 @@ def evaluate(
     repetition_penalty=2.0,
     **kwargs,
 ):
-    prompt = generate_prompt(input)
-    inputs = tokenizer(prompt, return_tensors="pt")
+    inputs = tokenizer(input, return_tensors="pt")
     input_ids = inputs["input_ids"].to(device)
     generation_config = GenerationConfig(
         temperature=temperature,
@@ -139,63 +117,18 @@ def evaluate(
         **kwargs,
     )
     with torch.no_grad():
-        if args.use_typewriter:
-            for generation_output in model.stream_generate(
-                input_ids=input_ids,
-                generation_config=generation_config,
-                return_dict_in_generate=True,
-                output_scores=False,
-                repetition_penalty=float(repetition_penalty),
-            ):
-                outputs = tokenizer.batch_decode(generation_output)
-                show_text = "\n--------------------------------------------\n".join(
-                    [output.split("### Response:")[1].strip().replace('�','')+" ▌" for output in outputs]
-                )
-                # if show_text== '':
-                #     yield last_show_text
-                # else:
-                yield show_text
-            yield outputs[0].split("### Response:")[1].strip().replace('�','')
-        else:
-            generation_output = model.generate(
-                input_ids=input_ids,
-                generation_config=generation_config,
-                return_dict_in_generate=True,
-                output_scores=False,
-                repetition_penalty=1.3,
-            )
-            output = generation_output.sequences[0]
-            output = tokenizer.decode(output).split("### Response:")[1].strip()
-            print(output)
-            yield output
-
-
-gr.Interface(
-    fn=evaluate,
-    inputs=[
-        gr.components.Textbox(
-            lines=2, label="Input", placeholder="Tell me about alpacas."
-        ),
-        gr.components.Slider(minimum=0, maximum=1, value=0.1, label="Temperature"),
-        gr.components.Slider(minimum=0, maximum=1, value=0.75, label="Top p"),
-        gr.components.Slider(minimum=0, maximum=100, step=1, value=40, label="Top k"),
-        gr.components.Slider(minimum=1, maximum=10, step=1, value=4, label="Beams Number"),
-        gr.components.Slider(
-            minimum=1, maximum=2000, step=1, value=256, label="Max New Tokens"
-        ),
-        gr.components.Slider(
-            minimum=1, maximum=300, step=1, value=1, label="Min New Tokens"
-        ),
-        gr.components.Slider(
-            minimum=0.1, maximum=10.0, step=0.1, value=2.0, label="Repetition Penalty"
-        ),
-    ],
-    outputs=[
-        gr.inputs.Textbox(
-            lines=25,
-            label="Output",
+        generation_output = model.generate(
+            input_ids=input_ids,
+            generation_config=generation_config,
+            return_dict_in_generate=True,
+            output_scores=False,
+            repetition_penalty=1.3,
         )
-    ],
-    title="Chinese-Vicuna 中文小羊驼",
-    description="中文小羊驼由各种高质量的开源instruction数据集，结合Alpaca-lora的代码训练而来，模型基于开源的llama7B，主要贡献是对应的lora模型。由于代码训练资源要求较小，希望为llama中文lora社区做一份贡献。",
-).queue().launch(share=True)
+        output = generation_output.sequences[0]
+        output = tokenizer.decode(output).split("### Response:")[1].strip()
+        print(output)
+        yield output
+
+
+if __name__ == "__main__":
+
